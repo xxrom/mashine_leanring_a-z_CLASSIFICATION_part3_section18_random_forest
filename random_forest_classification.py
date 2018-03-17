1de1fe3e0b5181151dedd9c4490e006528878849
Nikita Chernyshov <xpom55@mail.ru>
2018-03-17 23:56:38 +0300

    standart steps, but kernel_svm better, but microsoft used random forest for kinect

--- random_forest_classification.py
+++ random_forest_classification.py
@@ -1,4 +1,12 @@
 # Random Forest Classification
+# армия Desicion trees , создаем 500 деревьев, потом каждому скармливаем
+# данные и во время предсказания мы опрашиваем все 500 деревьев
+# и смотри к какая катеогория перевешивает процентно
+# чем больше деревьев, тем меньше переобучения
+#  microsoft  kinect использовали деревья эти, чтобы определять
+#  положение тела человека
+# данный алгоритм в нашем случае работает хуже чем, тотже kernel_svm(колокол)
+
 
 # Importing the libraries
 import numpy as np
@@ -11,59 +19,62 @@ X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
+# чтобы код выполнился намного быстрее???
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
 
-# Fitting Random Forest Classification to the Training set
+# Fitting the classifire to the Training set
 from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+classifier = RandomForestClassifier(
+    n_estimators = 10,
+    criterion = 'entropy',
+    random_state = 0
+  )
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
-y_pred = classifier.predict(X_test)
+y_pred = classifier.predict(X_test) # предсказываем данные из X_test
 
-# Making the Confusion Matrix
+# Making the Confusion Matrix # узнаем насколько правильная модель
 from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
+cm = confusion_matrix(y_test, y_pred) # in console cm
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01)) # подготавливаем матрицу поля данных с шагом 0.01
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), alpha = 0.75, cmap = ListedColormap(('red', 'green'))) # раскрашиваем данные по полотну X1, X2
+plt.xlim(X1.min(), X1.max()) # границы для областей указываем?
 plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Random Forest Classification (Training set)')
+for i, j in enumerate(np.unique(y_set)): # все точки рисуем на полотне
+  plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+              c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Random Forest Classificaton (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
-plt.legend()
+plt.legend() # в правом верхнем углу рисует соотношение точек и из значений
 plt.show()
 
-# Visualising the Test set results
+# Visualising the Test set results (границы одинаковые test = train)
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01)) # подготавливаем матрицу поля данных с шагом 0.01
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), alpha = 0.75, cmap = ListedColormap(('red', 'green'))) # раскрашиваем данные по полотну X1, X2
+plt.xlim(X1.min(), X1.max()) # границы для областей указываем?
 plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
+for i, j in enumerate(np.unique(y_set)): # все точки рисуем на полотне
+  plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+              c = ListedColormap(('red', 'green'))(i), label = j)
 plt.title('Random Forest Classification (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
-plt.legend()
+plt.legend() # в правом верхнем углу рисует соотношение точек и из значений
 plt.show()
